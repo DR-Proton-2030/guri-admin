3b043842ba20ba980e5ff77ed24f175f2471123a
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-03-26 21:46:08 +0530

    error fixed_ img

--- components/sidebar/companies-dropdown.tsx
+++ components/sidebar/companies-dropdown.tsx
@@ -1,54 +1,55 @@
-import {Dropdown, Text} from '@nextui-org/react';
-import React, {useState} from 'react';
-import { AcmeIcon } from '../icons/acme-icon';
-import logo from '../assets/WhatsApp Image 2024-03-21 at 08.43.39.jpeg'
-import {AcmeLogo} from '../icons/acmelogo';
-import {BottomIcon} from '../icons/sidebar/bottom-icon';
-import {Box} from '../styles/box';
-import {Flex} from '../styles/flex';
-import Image from 'next/image';
+import { Dropdown, Text } from "@nextui-org/react";
+import React, { useState } from "react";
+import { AcmeIcon } from "../icons/acme-icon";
+import logo from "../assets/WhatsApp Image 2024-03-21 at 08.43.39.jpeg";
+import { AcmeLogo } from "../icons/acmelogo";
+import { BottomIcon } from "../icons/sidebar/bottom-icon";
+import { Box } from "../styles/box";
+import { Flex } from "../styles/flex";
+// import Image from 'next/image';
 
 interface Company {
-   name: string;
-   location: string;
-   logo: React.ReactNode;
-   icon: Object;
+  name: string;
+  location: string;
+  logo: React.ReactNode;
+  icon: Object;
 }
 
 export const CompaniesDropdown = () => {
-   const [company, setCompany] = useState<Company>({
-      name: 'Ghuri Gharangna.',
-      location: 'Punjab,India',
-      logo: <Image src={logo} alt="" height={200} width={200}/>,
-      icon: <img src={logo.toString()} alt="Logo" />
-   });
-   return (
-      <Box className=''>
-               <Flex align={'center'} css={{gap: '$7'}}>
-                  {company.logo}
-                  <Box>
-                     <Text
-                        h3
-                        size={'$xl'}
-                        weight={'medium'}
-                        css={{
-                           m: 0,
-                           color: '$accents9',
-                           lineHeight: '$lg',
-                           mb: '-$5',
-                        }}
-                     >
-                        {company.name}
-                     </Text>
-                     <Text
-                        span
-                        weight={'medium'}
-                        size={'$xs'}
-                        css={{color: '$accents8'}}>
-                        {company.location}
-                     </Text>
-                  </Box>
-               </Flex>
-            </Box>
-   );
+  const [company, setCompany] = useState<Company>({
+    name: "Ghuri Gharangna.",
+    location: "Punjab,India",
+    logo: "",
+    icon: <img src={logo.toString()} alt="Logo" />,
+  });
+  return (
+    <Box className="">
+      <Flex align={"center"} css={{ gap: "$7" }}>
+        {company.logo}
+        <Box>
+          <Text
+            h3
+            size={"$xl"}
+            weight={"medium"}
+            css={{
+              m: 0,
+              color: "$accents9",
+              lineHeight: "$lg",
+              mb: "-$5",
+            }}
+          >
+            {company.name}
+          </Text>
+          <Text
+            span
+            weight={"medium"}
+            size={"$xs"}
+            css={{ color: "$accents8" }}
+          >
+            {company.location}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
